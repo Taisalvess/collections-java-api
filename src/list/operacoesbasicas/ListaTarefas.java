88e909fd47bd9073540cd942199ec7da3e3dfa9b
Tais Alves <taisalvessilva97@gmail.com>
2026-02-06 14:42:12 -0300

    implementa Lista de Tarefas com operações básicas de List

---
+++ src/list/operacoesbasicas/ListaTarefas.java
@@ -0,0 +1,34 @@
+package list.operacoesbasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> tarefas;
+
+    public ListaTarefas() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        tarefas.removeIf(tarefa ->
+                tarefa.getDescricao().equalsIgnoreCase(descricao));
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefas.size();
+    }
+
+    public List<String> obterDescricoesTarefas() {
+        List<String> descricoes = new ArrayList<>();
+        for (Tarefa tarefa : tarefas) {
+            descricoes.add(tarefa.getDescricao());
+        }
+        return descricoes;
+    }
+}
